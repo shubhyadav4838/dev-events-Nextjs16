a1244a59234e548cc86b84c044c2e35ad7e85e61
Shubh Yadav <shubhyadav4838@gmail.com>
2026-05-30 21:38:56 +0530

    Implement PostHog

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import {cn} from "@/lib/utils";
 import LightRays from "@/components/LightRays";
 import Navbar from "@/components/Navbar";
+import {PostHogProvider} from "@/components/PostHogProvider";
+import {PostHogPageView} from "@/components/PostHogPageView";
+import {Suspense} from "react";
 
 const geist = Geist({subsets: ['latin'], variable: '--font-sans'});
 
@@ -33,6 +36,8 @@ export default function RootLayout({
             className={cn("min-h-screen", "antialiased", schibstedGrotesk.variable, martianMono.variable, "font-sans", geist.variable)}
         >
         <body className="min-h-full flex flex-col">
+        <PostHogProvider>
+        <Suspense fallback={null}><PostHogPageView/></Suspense>
         <Navbar/>
 
         <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
@@ -54,6 +59,7 @@ export default function RootLayout({
         <main>
             {children}
         </main>
+        </PostHogProvider>
         </body>
         </html>
     );
